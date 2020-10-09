dee63eb29cdd708e76eda07fbea40b40ac5eacdb
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-09 21:44:52 +0000

    Automatically backed up by Learn

--- spec/false_equivalency_spec.rb
+++ spec/false_equivalency_spec.rb
@@ -2,6 +2,7 @@ require_relative '../fix_using_tests/false_equivalency'
 
 describe "false_equivalency" do
   it get_user_input do
+    prompt_user
     expect(selection(1)).to eq("YUM YUM MUNCH MUNCH MUNCH")
     expect(selection(2)).to eq("HAM HAM HAM IN MY TUMMY")
   end
